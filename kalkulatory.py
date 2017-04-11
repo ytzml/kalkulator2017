79cd9c3fcbd46d4f1c92be165ecfd3c473d20787
jacek <jacek@c.com>
2017-04-11 19:25:28 +0200

    funkcja z pomocą gotowa do review

--- kalkulatory.py
+++ kalkulatory.py
@@ -2,5 +2,9 @@ def dodawanie():
 	a = int(input('Podaj pierwsza liczbe'))
 	b = int(input('Podaj pierwsza liczbe'))
 	print(a+b)
-	
+
+def get_help():
+	print('To jest prosty program kalkulatora. Wprowadź dwie liczby i zatwierdź enterem.')
+
+get_help()	
 dodawanie()
